a847908de81aa8864776b04afa3e39d818eb3385
Joao Vitor <viictorsouza97@gmail.com>
2022-04-14 21:10:21 -0300

    feat: capturing and handling errors

--- backend/src/app/controllers/CategoryController.js
+++ backend/src/app/controllers/CategoryController.js
@@ -4,36 +4,43 @@ class CategoryController {
   async index(request, response) {
     const { orderBy } = request.query;
     const categories = await CategoryRepository.findAll(orderBy);
+
     response.json(categories);
   }
 
   async show(request, response) {
     const { id } = request.params;
-
     const category = await CategoryRepository.findById(id);
 
     if (!category) {
-      return response.status(400).json({ error: 'ID not found' });
+      return response.status(404).json({ error: 'Category not found' });
     }
 
-    response.json(category);
+    return response.json(category);
   }
 
   async store(request, response) {
     const { name } = request.body;
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
+      return response.status(400).json({ error: 'Category name is required!' });
     }
 
-    const category = await CategoryRepository.create({ name });
+    const categoryExists = await CategoryRepository.findByCategoryName(name);
 
-    response.json(category);
+    if (categoryExists) {
+      return response.status(400).json({ error: 'Category already exists!' });
+    }
+
+    const category = await CategoryRepository.create({
+      name,
+    });
+
+    return response.json(category);
   }
 
   async update(request, response) {
     const { id } = request.params;
-
     const { name } = request.body;
 
     const categoryExists = await CategoryRepository.findById(id);
@@ -43,7 +50,7 @@ class CategoryController {
     }
 
     if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
+      return response.status(400).json({ error: 'Category name is required!' });
     }
 
     const categoryByName = await CategoryRepository.findByCategoryName(name);
@@ -54,14 +61,15 @@ class CategoryController {
 
     const category = await CategoryRepository.update(id, { name });
 
-    response.json(category);
+    return response.json(category);
   }
 
   async delete(request, response) {
     const { id } = request.params;
 
     await CategoryRepository.delete(id);
-    response.sendStatus(204);
+
+    return response.sendStatus(204);
   }
 }
 
